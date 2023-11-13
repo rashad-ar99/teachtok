d7a32ba6376cef6a1eb317586232e5b47060690e
rashadar-99 <rashadar99@gmail.com>
2023-11-13 14:10:51 +0530

    feat: update to include Home and SafeAreaView

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, StatusBar, Platform } from "react-native";
+import Home from "./screens/Home";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App: React.FC = () => {
+	return (
+		<>
+			<StatusBar translucent={true} />
+			<SafeAreaView style={styles.SafeArea}>
+				<Home />
+			</SafeAreaView>
+		</>
+	);
+};
+
+export default App;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	SafeArea: {
+		flex: 1,
+		backgroundColor: "white",
+		paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+	},
 });
